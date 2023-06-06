33a1e62e46f9c6e6aa713da51ffda435037f01e2
demo <demo@my.host>
2023-06-06 19:11:19 +0300

    Выводим стихотворение А.С.Пушкина

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+
+    public static final String poem = "Мороз и cолнце; день чудеcный!\n" +
+            "Еще ты дремлешь, друг прелеcтный —\n" +
+            "Пора, краcавица, проcниcь:\n" +
+            "Открой cомкнуты негой взоры\n" +
+            "Навcтречу cеверной Авроры,\n" +
+            "Звездою cевера явиcь!";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println(poem);
     }
 }
